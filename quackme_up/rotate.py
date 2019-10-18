ffc71e500c1abb8f274fc06bca00df959f4bc4a2
WhiteRose13 <pangiann134ever@gmail.com>
2019-10-18 20:35:56 +0300

    Fixing python script

--- quackme_up/rotate.py
+++ quackme_up/rotate.py
@@ -1,7 +1,7 @@
 INT_BITS = 8
-ROL = 4
-def leftRotate(n, d):
-    return (n << d) | (n >> (INT_BITS - d))
+ROR = 4
+def rightRotate(n, d):
+    return (n >> d) | (n << (INT_BITS - d))
 
 def keep8bits(number):
     return (((1 << 8) - 1) & number)
@@ -11,4 +11,4 @@ encrypted = "118020E0225372A101415520A0C025E33540659575003085C1"
 i = 0
 for i in range(0, len(encrypted), 2):
     n = (int(encrypted[i] + encrypted[i+1], 16) ^ 22)
-    print(chr(keep8bits(leftRotate(n, ROL))), end = '')
+    print(chr(keep8bits(rightRotate(n, ROR))), end = '')
