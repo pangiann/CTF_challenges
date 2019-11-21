245f7eed0c234964ef79dea8121eed857595bad6
WhiteRose13 <pangiann134ever@gmail.com>
2019-11-21 12:38:12 +0200

    changing folders

--- picoCTF_2018_write-up/README.md
+++
@@ -1,2 +0,0 @@
-# picoCTF-2018-writeup
-This repository serves as a solution for picoCTF_2018 reversing and binary exploitation problems
--- picoCTF_2018_write-up/be_quick_or_be_dead_1/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp be-quick-or-be-dead-1 be-quick-or-be-dead-1_patched
-chmod +x be-quick-or-be-dead-1_patched
-
-rasm2 'mov eax, 0xeb866516' | xxd -p -r | dd conv=notrunc of=be-quick-or-be-dead-1_patched bs=1 seek=$((0x7a9)) 2>/dev/null
-
-./be-quick-or-be-dead-1_patched | tail -1 
